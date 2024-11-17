f563e0506a79bb62108bf00fa782d308d5a0bf1e
Felop2299 <pipe-220899@hotmail.com>
2024-11-17 04:13:52 +0000

    generador excusas codigo nuevo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function generarExcusas() {
+    let whoSelected = who[getRandomNumber(who.length)];
+    let actionSelected = action[getRandomNumber(action.length)];
+    let whatSelected = what[getRandomNumber(what.length)];
+    let whenSelected = when[getRandomNumber(when.length)];
+    return `${whoSelected} ${actionSelected} ${whatSelected} ${whenSelected}.`;
+  }
+
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excusas").innerHTML = generarExcusas();
 };
